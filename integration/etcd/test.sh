9096cd19867b300f8ca6208cbd4666b38cddcf98
Manuel Alejandro de Brito Fontes <aledbf@gmail.com>
2015-08-26 23:07:50 -0300

    bug(etcd): error in getv in variables with a dash

--- integration/etcd/test.sh
+++ integration/etcd/test.sh
@@ -13,7 +13,7 @@ curl -L -X PUT http://127.0.0.1:4001/v2/keys/prefix/database/port -d value=3306
 curl -L -X PUT http://127.0.0.1:4001/v2/keys/prefix/database/username -d value=confd
 curl -L -X PUT http://127.0.0.1:4001/v2/keys/prefix/upstream/app1 -d value=10.0.1.10:8080
 curl -L -X PUT http://127.0.0.1:4001/v2/keys/prefix/upstream/app2 -d value=10.0.1.11:8080
-
+curl -L -X PUT http://127.0.0.1:4001/v2/keys/services/api-dev -d value=dev-api
 
 # Run confd
 confd --onetime --log-level debug --confdir ./integration/confdir --backend etcd --node 127.0.0.1:4001 --watch
